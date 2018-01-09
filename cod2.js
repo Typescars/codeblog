ae32d4bdb71ea4c2c2eba9bdeaadde5b0312e5a1
Types cars <30573093+Typescars@users.noreply.github.com>
2018-01-09 18:28:00 +0200

    Add files via upload

---
+++ cod2.js
@@ -0,0 +1,24 @@
+var configSummary = {
+    thumbnailSize: "w350", // Define the post thumbnail size
+    summaryLength: 250, // Define the summary length     
+    defaultView:'grid', // Default view Mode
+	postPerPage:6, // Number post every Page (work with PageNavi) 
+	numshowpage:3  
+};
+var Alldefaultconfig = {
+    MaxPost:5,
+	MoreText: "Load more posts",
+    monthName: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],	
+	viewMoreText: "View More About",
+	BackupImage: 'http://1.bp.blogspot.com/-htG7vy9vIAA/Tp0KrMUdoWI/AAAAAAAABAU/e7XkFtErqsU/s72-c/grey.gif', 
+	summaryLength:150,	
+	FirstImageSize:"w350",
+	ImageSize:"w100", 
+	slideshowSpeed:5000,
+	NewsTickerText:"Latest News",
+	RandomTitle:"Random Posts",
+	RecentTitle:"Recent Posts",
+	RelatedTitle:"Related Posts",
+    RelatedStyle:"carousel",
+	CommentMode:"blogger"
+};
